e5b1f921d840a6000244da359b5da1987031d863
jhon65496 <jhon65496@yandex.ru>
2024-02-13 19:13:09 +0300

    - NewApp+ServicesLayer+DAL+EntitiesLayer; - EntitiesLayer. Add entity Book; - EntitiesLayer. Add entity Seller;

---
+++ EntitiesLayerDll/EntityBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntitiesLayerDll
+{
+    public class EntityBase
+    {
+        public int ID { get; set; }
+        public string Name{ get; set; }
+    }
+}
